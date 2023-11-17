dbee2c88eeff99df2d015dc83bcd4bafb52ea66c
Hoffmann <hoffmannqueifer23@gmail.com>
2023-11-16 22:17:29 -0300

    updte seed, get game by id

--- src/main/java/com/devsuperior/dlist/controllers/GameController.java
+++ src/main/java/com/devsuperior/dlist/controllers/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devsuperior.dlist.dto.GameDTO;
 import com.devsuperior.dlist.dto.GameMinDTO;
 import com.devsuperior.dlist.services.GameService;
 
@@ -21,5 +23,11 @@ public class GameController {
 	public List<GameMinDTO> findAll(){
 		List<GameMinDTO> listgames = gameService.findAll();
 		return listgames;
+	}
+	
+	@GetMapping(value = "/{id}")
+	public GameDTO findById(@PathVariable Long id){
+		GameDTO game = gameService.findById(id);
+		return game;
 	}
 }
--- src/main/java/com/devsuperior/dlist/services/GameService.java
+++ src/main/java/com/devsuperior/dlist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dlist.dto.GameDTO;
 import com.devsuperior.dlist.dto.GameMinDTO;
 import com.devsuperior.dlist.entities.Game;
 import com.devsuperior.dlist.repositories.GameRepository;
@@ -15,6 +17,15 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game game = gameRepository.findById(id).get(); 
+		GameDTO dto = new GameDTO(game);
+		
+		return dto;
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> listGames = gameRepository.findAll();
 		List<GameMinDTO> dto = listGames.stream().map(x -> new GameMinDTO(x)).toList();
